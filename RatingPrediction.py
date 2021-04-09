98be63dd1e79c0d426b5b1c6d50661e23379ae7e
Natasha Trayers <ntrayers@gmail.com>
2021-04-09 16:20:03 -0500

    Add data

--- RatingPrediction.py
+++ RatingPrediction.py
@@ -5,3 +5,14 @@ Created on Fri Apr  9 15:42:00 2021
 @author: ntrayers
 """
 
+from sklearn.naive_bayes import GaussianNB
+import os
+
+directory = "./train/neg"
+x = []
+for file in os.listdir(directory):
+    print(file)
+    # with open("../train/neg/" + file, 'r') as f:
+        # x.append(f.read())
+        
+        
